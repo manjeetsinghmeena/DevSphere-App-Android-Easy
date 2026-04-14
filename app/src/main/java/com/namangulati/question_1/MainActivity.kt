cbe88a09ec5d3ab78a15135aa19d63f74c63b11d
manjeetsinghmeena <manjeetsinghmeena6@gmail.com>
2026-04-14 19:51:23 +0530

    made changes in main activity file

--- app/src/main/java/com/namangulati/question_1/MainActivity.kt
+++ app/src/main/java/com/namangulati/question_1/MainActivity.kt
@@ -8,13 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         fun calculateTax(income: Double, taxRate: Double): Pair<Double, Double> {
             // TODO: Implement tax calculation
-            return Pair(0.0, 0.0)
+             val tax = (taxRate/100.0)*income
+            val finalIncome = income - tax
+
+            return Pair(tax, finalIncome)
         }
     }
 
@@ -28,17 +32,20 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        // val etIncome      = 
-        // val etTaxRate     = 
-        // val btnCalculate  = 
-        // val tvTaxAmount   = 
-        // val tvFinalIncome = 
+         val etIncome = findViewById<EditText>(R.id.etIncome)
+         val etTaxRate = findViewById<EditText>(R.id.etTaxRate)
+         val btnCalculate = findViewById<Button>(R.id.btnCalculate)
+         val tvTaxAmount = findViewById<TextView>(R.id.tvTaxAmount)
+         val tvFinalIncome = findViewById<TextView>(R.id.tvFinalIncome)
 
         btnCalculate.setOnClickListener {
-            // val income 
-            // val rate   
+           val income = etIncome.text.toString().trim().toDoubleOrNull() ?: 0.0
+           val rate = etTaxRate.text.toString().trim().toDoubleOrNull() ?: 0.0
 
+            val result = calculateTax(income,rate)
             // TODO: Call calculateTax and update tvTaxAmount, tvFinalIncome
+            tvTaxAmount.text = "Tax Amount: ₹" + result.first.toString()
+            tvFinalIncome.text = "Final Income: ₹"+ result.second.toString()
         }
     }
 }
